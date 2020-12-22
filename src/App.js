f042bdfa543a1d0cbedc8c2cefe43fd05d8d16a8
Robert Gregg <hi@robertgregg.co.uk>
2020-12-22 22:11:48 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movies from './components/movies';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <main className="container">
+        <h1>Vid.io</h1>
+          <Movies />
+      </main>
+     );
+  }
+}
+ 
+
+export default App;
