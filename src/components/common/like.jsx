f3fd64105f5996f41d7b9358c99e2174a01f57e2
Robert Gregg <hi@robertgregg.co.uk>
2021-01-05 19:51:38 +0000

    cleaning up codeg

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Like = (props) => {
   let classes = "fa fa-heart";
   if (!props.liked === true) classes += "-o";
-
   return <i className={classes} aria-hidden="true" onClick={props.onClick}></i>;
 };
 
