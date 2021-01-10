826d8e10702c5f51a369b08cfc547d8345c33e6a
Your Namehi@robertgregg.co.uk <you@example.comhi@robertgregg.co.uk>
2021-01-10 13:48:18 +0000

    minor code removal

--- src/components/movieDetails.jsx
+++ src/components/movieDetails.jsx
@@ -4,31 +4,17 @@ import Form from "./common/form";
 
 class MovieDetails extends Form {
   state = { 
-      data: { 
-        title: "",
-        genre: "",
-        stock: "",
-        rate: "" 
-      }, 
-      errors: {}
+      
    }
 
-  schema = {
-    title: Joi.string().required().label('Title'),
-    genre: Joi.string().required().label('Genre'),
-    stock: Joi.number().required().min(0).max(100).label('Stock'),
-    rate:  Joi.number().required().min(0).max(10).label('Rate'),
-  } 
+  schema = {}    
 
   render() { 
     return ( 
       <div className="grid-col-1-3">
         <h1>Movie Form</h1>
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput('title', 'Title', 'text', this.props.match.params.title )}
-          {this.renderSelect('genre', 'Genre', 'select')}
-          {this.renderInput('stock', 'Number in Stock')}
-          {this.renderInput('rate', 'Rate')}
+          
         </form>
         <button
           className="btn btn-primary"
